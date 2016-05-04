bf80a0acb3066555616c9ef056a0666eb373310c
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2016-05-04 12:40:54 +0100

    initial commit

---
+++ app/lib/fonts.js
@@ -0,0 +1,11 @@
+(function() {
+    Alloy.Globals.fontMap = {
+        "home": "\ue900",
+        "pencil": "\ue901",
+        "plus": "\ue902",
+        "minus": "\ue903",
+        "cross": "\ue904",
+        "checkmark": "\e905"
+    };
+    Alloy.Globals.webFont = "icomoon"
+}());
